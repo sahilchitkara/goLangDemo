9fa0223db64f3d9df4c224f499cfa0c58f64ff86
Sahil Chitkara <sahil.chitkara91@gmail.com>
2014-07-07 15:33:36 +0530

    added sample program in main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+type Page struct {
+	FileName string
+	Data []byte
+}
+func (p *Page) saveFile() error{
+	filename := p.FileName + ".txt"
+	return ioutil.WriteFile(filename,p.Data,0600)
+} 
+
+func loadPage(name string)  (*Page, error) {
+	fileName := name + ".txt"
+	data,err :=ioutil.ReadFile(fileName)
+	if err !=nil {
+		return nil,err
+	}
+	return &Page{FileName: fileName, Data: data},nil
+}
+
+func main() {
+	p1 := &Page{FileName: "TestPage",Data: []byte("This is an Sample Demo")}
+	p1.saveFile()
+	p2,err :=loadPage("TestPage")
+	if err!=nil{
+		fmt.Println(string("Error occured"))
+	}else{
+		fmt.Println(string(p2.Data))	
+	}
+}
